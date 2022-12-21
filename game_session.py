df5e2146553b9ec124df85527b8379ce1aa3d877
migowskiD <100491913+migowskiD@users.noreply.github.com>
2022-12-21 14:55:10 +0100

    adding questions and update in creating/reading

--- game_session.py
+++ game_session.py
@@ -2,12 +2,15 @@ import os
 import sys
 from time import sleep
 
+from termcolor import colored
+
 
 class GameSession:
     def __init__(self, flashcard_set):
         self.flashcard_set = flashcard_set
 
     def session_start(self):
+        counter = 0
         for i in range(5):
             pile = self.flashcard_set.get_pile(i)
             # for j, (question, answer) in enumerate(pile.items()):
@@ -29,17 +32,20 @@ class GameSession:
                     print("Flashcard num " + str(index+1) + "/" + str(len(pile)))
                     keys = list(pile.keys())
                     key = keys[index]
-                    print(key)
+                    print(colored(key, 'yellow'))
                     input("Press enter to show answer:")
-                    print(pile[key])
+                    print(colored(pile[key], 'green'))
                     known = input("Did you answer correctly? Type yes(y) or no(n). If you want to save and quit type q")
                     if known == "y" or known == "Y" or known == "yes" or known == "Yes" or known == "YES":
                         self.flashcard_set.move_up(i, key)
                     elif known == "q" or known == "Q":
                         self.flashcard_set.save_files()
-                        sys.exit()
+                        return
                     else:
                         index = index + 1
+                    counter += 1
+                    if counter % 10 == 0:
+                        self.flashcard_set.save_files()
         print("You finished all flashcards from this set!")
         sleep(5)
         self.flashcard_set.save_files()
